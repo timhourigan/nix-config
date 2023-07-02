8b38306361aed627df1fb3191c8fa77933fb650b
Tim Hourigan <1819176+timhourigan@users.noreply.github.com>
2023-07-02 22:38:22 +0100

    tlp power config

--- hosts/x13/configuration.nix
+++ hosts/x13/configuration.nix
@@ -62,13 +62,27 @@
   };
 
   # Power management
+  # tlp - https://linrunner.de/tlp/settings
   services.tlp = {
     enable = true;
     settings = {
+      # CPU
+      # See options with `sudo tlp-stat -p`
       CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 0;
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      # Battery
+      # Start charging at 75%, stop charging at 85%
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 85;
+      RESTORE_THRESHOLDS_ON_BAT = 1;
+      # Platform
+      # See options with `sudo tlp-stat -p`
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
     };
   };
 
