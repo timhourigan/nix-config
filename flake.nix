5fca9c7e876f68e57e95342142b6991cf8f8fe37
Tim Hourigan <1819176+timhourigan@users.noreply.github.com>
2024-08-30 14:50:33 +0100

    Add opx7070 config

--- flake.nix
+++ flake.nix
@@ -22,6 +22,9 @@
   outputs = { nixpkgs, home-manager, ... }@inputs: {
     # NixOS Configurations
     nixosConfigurations = {
+      opx7070 = nixpkgs.lib.nixosSystem {
+        modules = [ ./hosts/opx7070/configuration.nix ];
+      };
       x1c = nixpkgs.lib.nixosSystem {
         modules = [ ./hosts/x1c/configuration.nix ];
       };
@@ -31,6 +34,12 @@
     };
 
     # Home-Manager Configurations
+    homeConfigurations = {
+      "timh@opx7070" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [ ./home/home.nix ];
+        extraSpecialArgs = { inherit inputs; };
+      };
     homeConfigurations = {
       "timh@x1c" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
