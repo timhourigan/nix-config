83f8d54bae28ba6653e41886d0aa5cbbb6ba9ed4
Tim Hourigan <1819176+timhourigan@users.noreply.github.com>
2024-08-31 14:43:12 +0100

    No need for powersave on PC

--- hosts/opx7070/hardware-configuration.nix
+++ hosts/opx7070/hardware-configuration.nix
@@ -32,6 +32,5 @@
   # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
