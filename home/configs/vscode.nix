62b91d8fd5b9301bc5881c23290186babd184405
Tim Hourigan <1819176+timhourigan@users.noreply.github.com>
2024-12-15 11:31:05 +0000

    Update rust extension

--- home/configs/vscode.nix
+++ home/configs/vscode.nix
@@ -13,7 +13,7 @@
       yzhang.markdown-all-in-one # Markdown
       bbenoist.nix # Nix
       ms-python.python # Python
-      matklad.rust-analyzer # Rust
+      rust-lang.rust-analyzer # Rust
       hashicorp.terraform # Terraform
       redhat.vscode-yaml # YAML
       # Git
