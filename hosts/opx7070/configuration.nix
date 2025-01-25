45a85670bbac7de3023248701e3d1c020bb35910
Tim Hourigan <1819176+timhourigan@users.noreply.github.com>
2025-01-25 12:19:20 +0000

    Update to HA 2025.1

--- hosts/opx7070/configuration.nix
+++ hosts/opx7070/configuration.nix
@@ -122,7 +122,7 @@
           "--network=host"
         ];
         # https://github.com/home-assistant/core/releases
-        image = "ghcr.io/home-assistant/home-assistant:2024.12.4";
+        image = "ghcr.io/home-assistant/home-assistant:2025.1.4";
       };
     };
   };
