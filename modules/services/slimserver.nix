98bb38256b7d2bb662282d5e7bf4b0c8b27a11db
Tim Hourigan <1819176+timhourigan@users.noreply.github.com>
2025-02-23 21:47:06 +0000

    Start of Slimserver

---
+++ modules/services/slimserver.nix
@@ -0,0 +1,34 @@
+{ lib, config, options, pkgs, ... }:
+
+# SlimServer aka Logitech Media Server (LMS) aka Lyrion
+# https://mynixos.com/options/services.slimserver
+# https://lyrion.org/
+
+let
+  cfg = config.modules.services.slimserver;
+in
+{
+  options = {
+    modules.services.slimserver = {
+      enable = lib.mkEnableOption "Enable SlimServer";
+      package = lib.mkOption {
+        type = lib.types.package;
+        default = pkgs.slimserver;
+        description = "The package to use";
+      };
+      dataDir = lib.mkOption {
+        type = lib.types.path;
+        default = "/var/lib/slimserver";
+        description = "The directory to store the data";
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.slimserver = {
+      enable = true;
+      package = cfg.package;
+      dataDir = cfg.dataDir;
+    };
+  };
+}
