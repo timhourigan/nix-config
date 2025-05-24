d81b0883a17e6208dc08d6e17194bad7c18addb9
Tim Hourigan <1819176+timhourigan@users.noreply.github.com>
2025-05-24 21:09:00 +0100

    Allow sudo on PiHole API

--- hosts/mm/configuration.nix
+++ hosts/mm/configuration.nix
@@ -66,12 +66,14 @@
           "--cap-add=NET_BIND_SERVICE" # Allow binding to ports below 1024
           "--dns=9.9.9.9" # Container DNS
         ];
-        # Use Unbound, accessing via Podman interface
-        environment.FTLCONF_dns_upstreams = "10.88.0.1#5335";
-        # Needed when using container bridged mode
-        environment.FTLCONF_dns_listeningMode = "all";
+        environment = {
+          # Use Unbound, accessing via Podman interface
+          FTLCONF_dns_upstreams = "10.88.0.1#5335";
+          # Needed when using container bridged mode
+          FTLCONF_dns_listeningMode = "all";
+          # FTLCONF_webserver_api_password = "use-to-set-initial-password";
+        };
         environmentFiles = [ config.sops.secrets."pihole_env".path ];
-        # environment.FTLCONF_webserver_api_password = "use-to-set-initial-password";
         image = "docker.io/pihole/pihole:2025.04.0";
       };
       nebulaSync = {
