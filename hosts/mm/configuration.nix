b41ec32ed2dcd97fe99c4bd4b58ee1b34ceacfa8
Tim Hourigan <1819176+timhourigan@users.noreply.github.com>
2026-01-13 22:14:05 +0000

    Update PiHole to 2025.11.1

--- hosts/mm/configuration.nix
+++ hosts/mm/configuration.nix
@@ -56,7 +56,7 @@
           # FTLCONF_webserver_api_password = "use-to-set-initial-password";
         };
         environmentFiles = [ config.sops.secrets."pihole_env".path ];
-        image = "docker.io/pihole/pihole:2025.11.0";
+        image = "docker.io/pihole/pihole:2025.11.1";
       };
       nebulaSync = {
         enable = true;
