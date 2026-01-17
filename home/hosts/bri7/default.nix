780d2505873b6a5b7a367dd1fd4efe46dd0a4010
Tim Hourigan <1819176+timhourigan@users.noreply.github.com>
2026-01-17 10:17:07 +0000

    Switch bri7 Desktop

--- home/hosts/bri7/default.nix
+++ home/hosts/bri7/default.nix
@@ -1,10 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  # Packages to be installed
+  home.packages = with pkgs; [
+    # Apps
+    audacity # Audio editor
+    avidemux # Video editor
+  ];
+
+  # Programs and configurations to be installed
   imports = [
     ../../../modules/home
+    ../../configs/abcde.nix
   ];
 
   # Modules
-  modules = { };
+  modules.home = {
+    polybar.enable = true;
+    rofi.enable = true;
+    vscode.enable = true;
+  };
 }
