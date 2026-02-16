ac53e38d4bd90424296cf60d4345a7534177400e
Tim Hourigan <1819176+timhourigan@users.noreply.github.com>
2026-02-16 11:10:56 +0000

    Restore to Home Assistant

--- hosts/opx7070/configuration.nix
+++ hosts/opx7070/configuration.nix
@@ -98,7 +98,7 @@ in
           "--dns=9.9.9.9" # WORKAROUND - HA can start before DNS is up on boot
         ];
         # https://github.com/home-assistant/core/releases
-        image = "ghcr.io/home-assistant/home-assistant:2026.1.3";
+        image = "ghcr.io/home-assistant/home-assistant:2026.2.2";
         # https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/zi/zigbee2mqtt/package.nix
         z2mPackage = pkgs.pinned.zigbee2mqtt;
       };
