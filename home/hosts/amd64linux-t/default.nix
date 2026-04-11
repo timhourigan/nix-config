b87f7dbf060e793b62c23e96d6ed48cd37343a68
Tim Hourigan <1819176+timhourigan@users.noreply.github.com>
2026-04-11 22:21:43 +0000

    Tweak zsh settings for paths

--- home/hosts/amd64linux-t/default.nix
+++ home/hosts/amd64linux-t/default.nix
@@ -4,8 +4,8 @@
   # Match the distrobox container's custom home directory
   home.homeDirectory = lib.mkForce "/home/${config.home.username}/distrobox/amd64linux-t";
 
-  # Skip compinit insecure directory check (Nix store paths in distrobox)
-  programs.zsh.completionInit = "autoload -U compinit && compinit -u";
+  # Ignore insecure completion directories (Nix store paths in distrobox)
+  programs.zsh.completionInit = "autoload -U compinit && compinit -i";
 
   # Modules
   modules.home = {
