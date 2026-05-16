c88fdc9574accc0147932a25f2f3bfd9b0bb05dc
Tim Hourigan <1819176+timhourigan@users.noreply.github.com>
2026-05-16 15:01:12 +0100

    Return to unstable

--- hosts/opx7070/configuration.nix
+++ hosts/opx7070/configuration.nix
@@ -110,7 +110,14 @@ in
         image = "ghcr.io/home-assistant/home-assistant:2026.4.4";
 
       };
-      music-assistant.enable = true;
+      music-assistant = {
+        enable = true;
+        providers = [
+          "chromecast"
+          "dlna"
+          "squeezelite"
+        ];
+      };
       mosquitto.enable = true;
       zigbee2mqtt = {
         enable = true;
