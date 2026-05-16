5730d51b2f4b98e96680fc56aa71ebf53d4d41d7
Tim Hourigan <1819176+timhourigan@users.noreply.github.com>
2026-05-16 13:19:42 +0100

    Add protos

--- hosts/opx7070/configuration.nix
+++ hosts/opx7070/configuration.nix
@@ -110,7 +110,15 @@ in
         image = "ghcr.io/home-assistant/home-assistant:2026.4.4";
 
       };
-      music-assistant.enable = true;
+      music-assistant = {
+        enable = true;
+        providers = [
+          "airplay"
+          "chromecast"
+          "dlna"
+          "slimproto"
+        ];
+      };
       mosquitto.enable = true;
       zigbee2mqtt = {
         enable = true;
